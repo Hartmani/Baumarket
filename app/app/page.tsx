26599985695ca5b1ec01986793cd958dbaac1824
Hartmani <hartmanihartman@gmail.com>
2026-05-24 22:49:31 +0200

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,1 +1,25 @@
+export default function Home() {
 
+  return (
+
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+
+      <h1>Baumarket</h1>
+
+      <p>
+
+        Spojujeme stavebníky ve Středočeském kraji.
+
+      </p>
+
+      <a href="/request">
+
+        Vytvořit poptávku
+
+      </a>
+
+    </main>
+
+  );
+
+}
