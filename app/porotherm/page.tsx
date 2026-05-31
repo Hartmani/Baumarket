0c127ec462b57c1ec31c74670d3325e8100d1604
Hartmani <hartmanihartman@gmail.com>
2026-05-31 13:43:25 +0200

    Create page.tsx

---
+++ app/porotherm/page.tsx
@@ -0,0 +1,15 @@
+export default function PorothermPage() {
+
+  return (
+
+    <main style={{ padding: "20px" }}>
+
+      <h1>🧱 Porotherm</h1>
+
+      <p>Hromadný nákup cihel Porotherm.</p>
+
+    </main>
+
+  );
+
+}
