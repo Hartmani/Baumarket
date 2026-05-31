2ea06cb4e72f0b0d922cfb062242b672a99ea196
Hartmani <hartmanihartman@gmail.com>
2026-05-31 16:53:55 +0200

    Create page.tsx

---
+++ app/rezivo/page.tsx
@@ -0,0 +1,31 @@
+export default function RezivoPage() {
+
+  return (
+
+    <main style={{ padding: "20px" }}>
+
+      <h1>🪵 Střešní řezivo</h1>
+
+      <p>Hromadné nákupy stavebního a střešního řeziva za výhodné ceny.</p>
+
+      <ul>
+
+        <li>Střešní latě</li>
+
+        <li>Kontralatě</li>
+
+        <li>KVH hranoly</li>
+
+        <li>BSH lepené hranoly</li>
+
+        <li>Prkna a fošny</li>
+
+        <li>Konstrukční řezivo</li>
+
+      </ul>
+
+    </main>
+
+  );
+
+}
