5c334dc39a49b1e014ab4d1e3dd513feacd024ce
Hartmani <hartmanihartman@gmail.com>
2026-05-31 16:52:08 +0200

    Create page.tsx

---
+++ app/sadrokarton/page.tsx
@@ -0,0 +1,31 @@
+export default function SadrokartonPage() {
+
+  return (
+
+    <main style={{ padding: "20px" }}>
+
+      <h1>🧱 Sádrokarton</h1>
+
+      <p>Hromadné nákupy sádrokartonových systémů za výhodné ceny.</p>
+
+      <ul>
+
+        <li>Sádrokartonové desky</li>
+
+        <li>Profily CW a UW</li>
+
+        <li>Profily CD a UD</li>
+
+        <li>Tmely a pásky</li>
+
+        <li>Minerální izolace</li>
+
+        <li>Spojovací materiál</li>
+
+      </ul>
+
+    </main>
+
+  );
+
+}
