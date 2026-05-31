ff043e6bb01872fb1ef448073a9c11a920a01e67
Hartmani <hartmanihartman@gmail.com>
2026-05-31 16:44:31 +0200

    Create page.tsx

---
+++ app/strecha/page.tsx
@@ -0,0 +1,27 @@
+export default function StrechaPage() {
+
+  return (
+
+    <main style={{ padding: "20px" }}>
+
+      <h1>🏠 Střešní krytiny</h1>
+
+      <p>Hromadné nákupy střešních krytin za výhodnější ceny.</p>
+
+      <ul>
+
+        <li>Tondach</li>
+
+        <li>Bramac</li>
+
+        <li>KM Beta</li>
+
+        <li>Prefa</li>
+
+      </ul>
+
+    </main>
+
+  );
+
+}
