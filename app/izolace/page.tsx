564d17107a0e5513485ae611703cad7f824876c7
Hartmani <hartmanihartman@gmail.com>
2026-05-31 16:48:57 +0200

    Create page.tsx

---
+++ app/izolace/page.tsx
@@ -0,0 +1,29 @@
+export default function IzolacePage() {
+
+  return (
+
+    <main style={{ padding: "20px" }}>
+
+      <h1>🧱 Izolace</h1>
+
+      <p>Hromadné nákupy tepelných a stavebních izolací.</p>
+
+      <ul>
+
+        <li>EPS polystyren</li>
+
+        <li>XPS polystyren</li>
+
+        <li>Minerální vata</li>
+
+        <li>Foukaná izolace</li>
+
+        <li>PIR desky</li>
+
+      </ul>
+
+    </main>
+
+  );
+
+}
